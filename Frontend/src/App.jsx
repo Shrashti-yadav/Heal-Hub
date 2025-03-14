a89e22fe56e30895b8523edfe04f918d85e4f748
Kratika Singh <kratikaarun@gmail.com>
2025-03-14 15:18:25 +0530

    Resolved merge conflict in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import { Route ,Routes } from 'react-router-dom';
-import Home from './pages/Home'
-import Header from './components/Header';
-// import Appointment from './pages/Appointment';
+import Home from './pages/Home';
+// import Appointment from './pages/Appointment'; // Uncomment only if needed
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-    <Navbar/>
-    <Routes>
-      <Route path='/'element={<Home/>}/>
-      {/* <Route path='/appointment/:docId' element={<Appointment/>} /> */}
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route path='/appointment/:docId' element={<Appointment />} /> */}
+      </Routes>
     </div>
   );
 };
