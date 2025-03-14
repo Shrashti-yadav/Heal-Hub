da0ca56198a30f8f1a4189bcc827c1bb51ffda32
Kratika Singh <kratikaarun@gmail.com>
2025-03-14 15:44:59 +0530

    ui change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
-// import Appointment from './pages/Appointment'; // Uncomment only if needed
+import Doctors from './pages/Doctors';
+import { AppContext } from './context/AppContext'; // Adjust path as needed
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
+    <div className="mx-4 sm:mx-[10%]">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        {/* <Route path='/appointment/:docId' element={<Appointment />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />}/>
       </Routes>
+      <Footer />
     </div>
   );
 };
