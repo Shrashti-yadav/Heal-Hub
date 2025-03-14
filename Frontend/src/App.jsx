7578b1bf12ac1467df25ceaac133d3401e992e00
Kratika Singh <kratikaarun@gmail.com>
2025-03-14 14:51:51 +0530

    resolve error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { Route ,Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import Header from './components/Header';
+// import Appointment from './pages/Appointment';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
     <Navbar/>
     <Routes>
       <Route path='/'element={<Home/>}/>
-      
+      {/* <Route path='/appointment/:docId' element={<Appointment/>} /> */}
     </Routes>
     </div>
   )
