c0efbbd93668dc4bd831ee0d5fab55dab840ed38
shrashti-yadav <shrashtiyadav188@gmail.com>
2025-03-16 22:00:53 +0530

    database and api to add doctor

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDB from './config/mongodb.js'
+import connectCloudinary  from './config/cloudinary.js'
+import adminRouter from './routes/adminroute.js'
 const app=express()
 const port=process.env.PORT||4000;
+connectDB()
+connectCloudinary()
 
+//middlewares
 app.use(express.json())
 app.use(cors())
 
+
+//api endpoints
+app.use('/api/admin',adminRouter)
+//localhost:4000/api/admin
 app.get("/",(req,res)=>{
     res.send("api work")
 })
