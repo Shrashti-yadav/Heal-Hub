64cac429ef7e6d5f08d00cbc266e93308bff55f9
Shourya <shouryamishra55@gmail.com>
2025-03-17 13:26:53 +0530

    Database Connection fix

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,9 +1,25 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
-  mongoose.connection.on('connected',()=>console.log("Database Connected"))
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/Heal-Hub`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/Heal-Hub`)
+    console.log("Database Connected");
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB Connection Error:", err);
+    });
+
+  } catch (error) {
+    console.log(process.env.MONGODB_URI)
+    console.error("Database Connection Failed:", error);
+    process.exit(1); 
+  }
 };
 
-export default connectDB
+export default connectDB;
