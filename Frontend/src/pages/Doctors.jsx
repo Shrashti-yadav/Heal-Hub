fb842cea239e94e26b912c50e1b50260b05e8d57
shrashti-yadav <shrashtiyadav188@gmail.com>
2025-03-18 07:44:00 +0530

    Myprofile and Myappointments pages done

--- Frontend/src/pages/Doctors.jsx
+++ Frontend/src/pages/Doctors.jsx
@@ -5,6 +5,7 @@ import { AppContext } from '../context/AppContext';
 const Doctors = () => {
   const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter,setShowFilter]=useState(false);
   const navigate = useNavigate();
   const { doctors } = useContext(AppContext);
 
@@ -24,8 +25,9 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Browse through the doctor's specialists.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white':''}`} onClick={()=>setShowFilter(prev => !prev)}>Filters</button>
         {/* Speciality List */}
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex':'hidden sm:flex'}`}>
           {['General Physician', 'Gynecologist', 'Dermatologist', 'Pediatrician', 'Neurologist', 'Gastroenterologist','Cardiologist','Orthopedic','ENT','Ophthalmologist','Urologist','Dentist','Oncologist','Psychiatrist'].map((spec) => (
             <p
               key={spec}
